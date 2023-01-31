b9f36867cd651375bb92706b27df365eb4806fed
Alan.E.corimayo <Alan.E.corimayo@gmail.com>
2023-01-31 14:42:42 -0300

    mi primer commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Bienvenidos division 1");
+	alert("Esto funciona de maravilla");
 }
 
